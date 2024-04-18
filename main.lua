7e1d2860f39ba127eddd683e0c88f46788669f7d
Klehrik <78520710+Klehrik@users.noreply.github.com>
2024-04-18 14:22:51 -0400

    v1.0.1

--- main.lua
+++ main.lua
@@ -1,12 +1,12 @@
--- No Wall Clip v1.0.0
+-- No Wall Clip v1.0.1
 -- Klehrik
 
 log.info("Successfully loaded ".._ENV["!guid"]..".")
 mods.on_all_mods_loaded(function() for k, v in pairs(mods) do if type(v) == "table" and v.hfuncs then Helper = v end end end)
 
 local player = nil
-local last_x = 0
-local last_y = 0
+local last_x = nil
+local last_y = nil
 
 
 
@@ -14,11 +14,19 @@ local last_y = 0
 
 gm.post_script_hook(gm.constants.__input_system_tick, function()
     if Helper.instance_exists(player) then
+        -- Reset saved position when falling out of the room
+        if player.y >= gm.variable_global_get("room_height") then
+            last_x = nil
+            return
+        end
+
         -- Wall collision detected
-        local in_wall = gm.collision_line(player.x, player.y, last_x, last_y, gm.constants.pBlockStatic, false, true) ~= -4.0
-        if in_wall and not (player.activity == 92.0 and player.activity_flags == 0.0) then
-            player.x, player.y = last_x, last_y
-            player.pHspeed = 0.0
+        if last_x ~= nil then
+            local in_wall = gm.collision_line(player.x, player.y, last_x, last_y, gm.constants.pBlockStatic, false, true) ~= -4.0
+            if in_wall and not (player.activity == 92.0 and player.activity_flags == 0.0) then
+                player.x, player.y = last_x, last_y
+                player.pHspeed = 0.0
+            end
         end
 
         last_x, last_y = player.x, player.y
